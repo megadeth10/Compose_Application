74ea8c976dc2908a4df11f8eac3f38e53bfa7118
이상현 <sanghyun.lee@deleo.co.kr>
2022-08-23 12:14:31 +0900

    화면 Width를 이용한 layout 재배치 코드 추가함.

--- app/src/main/java/com/my/composeapplication/scene/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/my/composeapplication/scene/ConstraintLayoutActivity.kt
@@ -1,14 +1,16 @@
 package com.my.composeapplication.scene
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import com.my.composeapplication.base.BaseComponentActivity
@@ -25,8 +27,8 @@ class ConstraintLayoutActivity : BaseComponentActivity() {
 
 @Composable
 fun ConstraintLayoutContent() {
-    Column {
-        Row() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.fillMaxWidth()) {
             ConstraintLayout {
                 val (button1, button2, text1) = createRefs()
 
@@ -58,31 +60,39 @@ fun ConstraintLayoutContent() {
                 }
             }
         }
-        Row() {
-            val constraintSet = buttonTextConstraintSet()
-            ConstraintLayout(constraintSet) {
-                Button(
-                    onClick = {},
-                    modifier = Modifier.layoutId("button3")
-                ) {
-                    Text("Button1")
-                }
-                Text(
-                    text = "Text",
-                    modifier = Modifier.layoutId("text2")
-                )
-                Button(
-                    onClick = {},
-                    modifier = Modifier.layoutId("button4")
-                ) {
-                    Text("Button2")
+        BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
+            Log.e(ConstraintLayoutActivity::class.simpleName, "ConstraintLayoutContent() minwidth: $minWidth")
+            // TODO Compose의 width를 이용하여 compose의 배치를 자유롭게 변경한다.
+            val constraintSet = if (minWidth < 600.dp) {
+                portraitConstraintSet()
+            } else {
+                landScapeConstraintSet()
+            }
+            Row() {
+                ConstraintLayout(constraintSet) {
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.layoutId("button3")
+                    ) {
+                        Text("Button1")
+                    }
+                    Text(
+                        text = "Text",
+                        modifier = Modifier.layoutId("text2")
+                    )
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.layoutId("button4")
+                    ) {
+                        Text("Button2")
+                    }
                 }
             }
         }
     }
 }
 
-private fun buttonTextConstraintSet():ConstraintSet {
+private fun portraitConstraintSet() : ConstraintSet {
     return ConstraintSet {
         val button3 = createRefFor("button3")
         val button4 = createRefFor("button4")
@@ -104,8 +114,78 @@ private fun buttonTextConstraintSet():ConstraintSet {
     }
 }
 
+private fun landScapeConstraintSet() : ConstraintSet {
+    return ConstraintSet {
+        val button3 = createRefFor("button3")
+        val button4 = createRefFor("button4")
+        val text2 = createRefFor("text2")
+
+        constrain(button3) {
+            top.linkTo(parent.top, margin = 16.dp)
+        }
+        constrain(button4) {
+            top.linkTo(button3.top)
+            start.linkTo(button3.end)
+        }
+        constrain(text2) {
+            top.linkTo(button4.top)
+            start.linkTo(button4.end)
+            end.linkTo(parent.end)
+        }
+    }
+}
+
+@Composable
+fun TwoTexts(
+    text1: String,
+    text2: String,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        // TODO lineHeight를 이용하여 lineSpacing을 해결할수 있다.
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.End)
+                .wrapContentHeight(Alignment.CenterVertically),
+            text = text1,
+            lineHeight = 20.sp,
+            fontSize = 20.sp,
+            textAlign = TextAlign.End
+        )
+        Divider(
+            color = Color.Black,
+            modifier = Modifier.fillMaxHeight().width(1.dp)
+        )
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    MaterialTheme {
+        Surface {
+            TwoTexts(text1 = "Hi\nasfasdfasdf\nasdfasdf", text2 = "there")
+        }
+    }
+}
+
 @Preview
 @Composable
 fun ConstraintLayoutPreview() {
     ConstraintLayoutContent()
+}
+
+@Preview
+@Composable
+fun testPreview() {
+    Text(text = "aaaaa")
 }
