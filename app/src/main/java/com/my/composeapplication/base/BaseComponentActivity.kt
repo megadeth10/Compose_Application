4dc1fbd9f5fe80e4ce8c9afbb1645be816ed4339
이상현 <sanghyun.lee@deleo.co.kr>
2022-08-26 12:27:24 +0900

    AOS API 33 onBackPress deprecated로 backpress call back 추가함.

--- app/src/main/java/com/my/composeapplication/base/BaseComponentActivity.kt
+++ app/src/main/java/com/my/composeapplication/base/BaseComponentActivity.kt
@@ -2,6 +2,7 @@ package com.my.composeapplication.base
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 
@@ -10,10 +11,23 @@ import androidx.compose.ui.platform.ComposeView
  */
 abstract class BaseComponentActivity : ComponentActivity() {
     abstract fun getContent() : @Composable () -> Unit
+    protected var backPressCallback : (() -> Unit)? = null
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            backPressCallback?.invoke() ?: finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
         setContentView(ComposeView(this).apply {
             this.setContent(getContent())
         })
+    }
+
+    override fun onDestroy() {
+        this.onBackPressedCallback.remove()
+        super.onDestroy()
     }
 }
